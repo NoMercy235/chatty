aa0461302aa6aac10f5bb1de0d395d5dc113436e
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2020-06-03 13:35:53 +0300

    feat: style the chat tab

--- frontend/src/components/ChatTab/ChatTab.jsx
+++ frontend/src/components/ChatTab/ChatTab.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import * as styles from './ChatTab.module.scss';
 
 export const ChatTab = () => {
+  const [message, setMessage] = useState('');
+
+  const onMessageChange = e => setMessage(e.currentTarget.value);
+
   return (
-    <span>TODO</span>
+    <div className={styles.container}>
+      <div className={styles.messagesContainer}>messages</div>
+      <textarea
+        id="message"
+        rows={3}
+        className={styles.textArea}
+        placeholder="Write a message"
+        value={message}
+        onChange={onMessageChange}
+      />
+    </div>
   );
 };
