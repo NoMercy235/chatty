62cfaebf9735c42d83049f554f7ae85308430440
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2020-06-07 14:26:00 +0300

    feat: visual styling for users that can be selected for encrypted chat

--- frontend/src/components/UsersTab/UsersTab.jsx
+++ frontend/src/components/UsersTab/UsersTab.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as classNames from 'classnames';
 
 import { UserEntry } from '../UserEntry/UserEntry';
 import { MEETINGBOT_USER_ID } from '../../shared/constants';
@@ -6,17 +7,21 @@ import { MEETINGBOT_USER_ID } from '../../shared/constants';
 import * as styles from './UsersTab.module.scss';
 
 export const UsersTab = ({ currentUser, users, onUserClick }) => {
+  const isMe = (user) => user.id === currentUser.id;
+
+  const isSelectable = (user) => !isMe(user) && (user.id !== MEETINGBOT_USER_ID);
+
   const onHandleUserClick = (isMe, user) => {
-    !isMe && (user.id !== MEETINGBOT_USER_ID) && onUserClick(user);
+    isSelectable(user) && onUserClick(user);
   };
 
   return users.map(user => {
     return (
       <UserEntry
         key={user.id}
-        className={styles.userRow}
+        className={classNames(styles.userRow, { [styles.selectable]: isSelectable(user) })}
         user={user}
-        isMe={user.id === currentUser.id}
+        isMe={isMe(user)}
         onUserClick={onHandleUserClick}
       />
     );
